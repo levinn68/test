90fb90a04485ab3727a03880b6b02e94f194f419
levinn68 <spartamods77@gmail.com>
2026-01-13 01:02:31 +0700

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,10 @@
+// /assets/config.js
+export const SUPABASE_URL = "https://vhybkhalumqszuodpnfh.supabase.co";
+
+// PAKAI ANON KEY (JWT panjang yang role=anon). Ini memang public.
+export const SUPABASE_ANON_KEY =
+  "PASTE_ANON_KEY_KAMU_DI_SINI";
+
+// GSI Client ID (One Tap / button)
+export const GSI_CLIENT_ID =
+  "803291539340-6pdb7n2q88p8i2vcglskubbn5m65crur.apps.googleusercontent.com";
